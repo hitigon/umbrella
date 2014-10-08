add8f3658f5ce318e61110cb123369822dc6c41e
hitigon <hitigon@gmail.com>
2014-10-08 02:06:41 -0700

    Add chat list

---
+++ umbrella/ChatListViewController.h
@@ -0,0 +1,17 @@
+//
+//  ChatListViewController.h
+//  umbrella
+//
+//  Created by hitigon on 10/7/14.
+//  Copyright (c) 2014 umbrella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatListViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate>{
+}
+
+//@property(nonatomic, strong) IBOutlet UITableView *chatListView;
+
+@end
+
---
+++ umbrella/ChatListViewController.m
@@ -0,0 +1,51 @@
+//
+//  ChatListViewController.m
+//  umbrella
+//
+//  Created by hitigon on 10/7/14.
+//  Copyright (c) 2014 umbrella. All rights reserved.
+//
+
+#import "ChatListViewController.h"
+
+@interface ChatListViewController ()
+
+@end
+
+@implementation ChatListViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+//    NSLog(@"%d", self.chatListView.numberOfSections);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        [cell setAccessoryType:UITableViewCellAccessoryNone];
+    }
+    
+    [[cell textLabel] setText:@"test"];
+    
+    return cell;
+}
+
+@end
