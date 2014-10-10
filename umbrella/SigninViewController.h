c4908e650faa00953c8f29932a57f3157f30c839
hitigon <hitigon@gmail.com>
2014-10-09 23:18:32 -0700

    Test Commit

---
+++ umbrella/SigninViewController.h
@@ -0,0 +1,14 @@
+//
+//  SigninViewController.h
+//  umbrella
+//
+//  Created by hitigon on 10/8/14.
+//  Copyright (c) 2014 umbrella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SigninViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *username;
+
+@end
---
+++ umbrella/SigninViewController.m
@@ -0,0 +1,37 @@
+//
+//  SigninViewController.m
+//  umbrella
+//
+//  Created by hitigon on 10/8/14.
+//  Copyright (c) 2014 umbrella. All rights reserved.
+//
+
+#import "SigninViewController.h"
+
+@interface SigninViewController ()
+
+@end
+
+@implementation SigninViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"%@", self.username.placeholder);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
